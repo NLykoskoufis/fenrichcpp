68c9aa85a97bedab96c0879219f6821b6ae5f809
Nikolaos Lykoskoufis <nikolaoslykoskoufis@Nikolaoss-MacBook-Pro.local>
2020-12-09 09:21:49 +0100

    Upstream distance and downstream distance are read as strings and converted later to integer because of NAs

--- src/mode_analysis/analysis_process.cpp
+++ src/mode_analysis/analysis_process.cpp
@@ -21,7 +21,7 @@ void analysis_cpp::createNullDistribution(){
            // If they do not match, through message
            
            if(qtl_dist_phe_var[i] > 0){
-               if(downstream_distance[s].compare("NA") == 0) continue;
+               if(downstream_distance[s].compare("NA") != 0){
                if(qtl_dist_phe_var_from[i] <= stoi(downstream_distance[s]) && qtl_dist_phe_var_to[i]>= stoi(downstream_distance[s])){
                    if(qtl_maf_from[i] <= null_maf[s] && qtl_maf_to[i] >= null_maf[s]){
                        if(nominal[s] != 1 && find(nulldistribution.begin(),nulldistribution.end(), null_id[s]) == nulldistribution.end()){
@@ -29,8 +29,8 @@ void analysis_cpp::createNullDistribution(){
                        }
                    }
                }
-           }else{
-               if(upstream_distance[s].compare("NA") == 0) continue;
+           }}else{
+               if(upstream_distance[s].compare("NA") != 0){
                if(qtl_dist_phe_var_from[i] <= stoi(upstream_distance[s]) && qtl_dist_phe_var_to[i]>= stoi(upstream_distance[s]) {
                     if(qtl_maf_from[i] <= null_maf[s] && qtl_maf_to[i] >= null_maf[s]){
                         if(nominal[s] != 1 && find(nulldistribution.begin(),nulldistribution.end(),null_id[s]) == nulldistribution.end()){
@@ -39,6 +39,7 @@ void analysis_cpp::createNullDistribution(){
                     }
                 }
             }
+           }
         }// NULL for loop
         if(toRandomPeak.size() == 0){
             cout << "No variants found" << endl;
